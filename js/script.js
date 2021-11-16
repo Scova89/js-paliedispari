5a2eb23491d43fe978a34a6ae8d20eec05ce365a
Francesco <scovascova@gmail.com>
2021-11-16 17:29:59 +0100

    esercizio palindromo due soluzioni

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// Palidroma 
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+
+// Palindroma Prima soluzione
+
+// const insParola = prompt('Inserisci una parola');
+
+// invoco la funzione creata sotto
+// let parolaInvertita = invertiParola(insParola);
+
+// pongo la condizione per cui se la parola inserita è palindroma la stamperà nel console log
+// if(insParola == parolaInvertita){
+//     console.log('la parola è palindroma');
+//   } else {
+//     console.log('la parola non è palindroma');
+//   }
+  
+// creo una funzione nella quale uso tre metodi diversi per inserire spazi tra la parola, invertire e riunirla
+// function invertiParola(stringa){
+//   let stringaInversa = stringa.split('').reverse().join('');  
+//   return stringaInversa;
+// }
+
+
+// Palindroma Seconda soluzione
+// const insParola = prompt('Inserisci una parola');
+// if(palindroma(insParola) == true) {
+//     console.log('Questa parola è palindroma');
+// }else{
+//     console.log('Questa parola non è palindroma');
+// }
+
+// function palindroma(stringa){
+//     for(let i = 0; i < stringa.length; i++){
+//         if(stringa[i] != stringa[stringa.length - (1 + i)]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+
+
+// Pari e Dispari
+
+
